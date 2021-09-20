b18a11576de5c810fe9cb901ab38eaa36cb29c62
Elue-dev <eluewisdom@gmail.com>
2021-09-20 22:49:04 +0100

    Changes

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ const burger = document.querySelector (".fa-bars");
 burger.onclick = () => {
     burger.classList.toggle ("fa-times");
 }
+
+const emailInput = document.querySelector('#email');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    if(emailInput.value === ''){
+        clearAlert();
+        showAlert('Please enter an email before submission...', 'error');
+        e.preventDefault();
+    }else{
+        emailInput.value = '';
+    }
+})
+
+function showAlert(message, className){
+    var div = document.createElement("div");
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    document.querySelector(".insert").appendChild(div);
+   
+    setTimeout(() => div.remove(), 3000);
+}
+
+function clearAlert(){
+    let currentAlert = document.querySelector('.alert');
+        if(currentAlert){
+            currentAlert.remove();
+        }
+}
